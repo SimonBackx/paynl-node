7c92103b871b71c9f115e76a7621f9ada04f1c71
Simon Backx <hi@simonbackx.com>
2020-06-14 17:59:02 +0200

    Added verbose log

--- src/Paynl.ts
+++ src/Paynl.ts
@@ -56,6 +56,10 @@ export class Paynl {
             data["serviceId"] = this.serviceId;
 
             let jsonData = JSON.stringify(data);
+
+            if (this.verbose) {
+                console.log(jsonData);
+            }
             const req = https.request(
                 {
                     hostname: this.hostname,
